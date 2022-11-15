c58ac7af9a4da91d4c30a0d24d202a7c040dfd6f
ces0028 <ceshin0028@gmail.com>
2022-11-15 15:50:31 +0900

    [CREATE] PagerAdapterNote.kt

---
+++ app/src/main/java/kr/or/mrhi/apps/PagerAdapterNote.kt
@@ -0,0 +1,23 @@
+package kr.or.mrhi.apps
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PagerAdapterNote(activity: FragmentActivity) : FragmentStateAdapter(activity)  {
+    val fragmentList = ArrayList<Fragment>()
+    val titleList = ArrayList<String>()
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList.get(position)
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+}
